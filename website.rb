762f260644817b7af7a8637fa0b6bce985635787
Anders Bälter <anders@84codes.com>
2018-03-26 11:37:55 +0200

    downloas page-requisites

--- website.rb
+++ website.rb
@@ -23,7 +23,7 @@ class Website
     files.map! { |f| "http://localhost:#{port}/#{f.sub(%r{^/}, '')}" }
     File.write "Files", files.join("\n")
 
-    system "wget --mirror --input-file Files"
+    system "wget --mirror --page-requisites --input-file Files"
     Process.kill 'INT', pid
 
     FileUtils.mv "localhost:#{port}", "output"
